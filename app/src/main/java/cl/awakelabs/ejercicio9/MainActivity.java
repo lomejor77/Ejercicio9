3fdc68ee11db80aaf3f370ceb2f1c736e51ca1fe
Alex Muñoz <lomejor77@gmail.com>
2023-06-27 23:51:02 -0400

    Ejercicio 9 inicializada funciones de los botones

--- app/src/main/java/cl/awakelabs/ejercicio9/MainActivity.java
+++ app/src/main/java/cl/awakelabs/ejercicio9/MainActivity.java
@@ -2,13 +2,60 @@ package cl.awakelabs.ejercicio9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+        ImageButton btnUno = findViewById(R.id.btnUno);
+        ImageButton btnDos = findViewById(R.id.btnDos);
+        ImageButton btnTres = findViewById(R.id.btnTres);
+        ImageButton btnCuatro = findViewById(R.id.btnCuatro);
+
+        btnUno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveSecondActivity();
+            }
+        });
+
+        btnDos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveSecondActivity();
+            }
+        });
+
+        btnTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveSecondActivity();
+            }
+        });
+
+        btnCuatro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveSecondActivity();
+            }
+        });
+    }
+
+    public void moveSecondActivity(){
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/cl/awakelabs/ejercicio9/SecondActivity.java
+++ app/src/main/java/cl/awakelabs/ejercicio9/SecondActivity.java
@@ -9,6 +9,6 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second2);
+        setContentView(R.layout.activity_second);
     }
 }
