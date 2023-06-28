8f0d12f4aadf3b546211136007092015c41c9b96
Alex Muñoz <lomejor77@gmail.com>
2023-06-28 00:38:46 -0400

    Ejercicio 9 Funcionalidad completa

--- app/src/main/java/cl/awakelabs/ejercicio9/MainActivity.java
+++ app/src/main/java/cl/awakelabs/ejercicio9/MainActivity.java
@@ -11,6 +11,11 @@ import android.widget.ImageButton;
 @SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
 
+    //Obtencion estatica de las imagenes
+    int image1 = R.drawable.ic_launcher_foreground;
+    int image2 = R.drawable.baseline_assignment_ind_24;
+    int image3 = R.drawable.baseline_back_hand_24;
+    int image4 = R.drawable.baseline_balance_24;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,34 +33,35 @@ public class MainActivity extends AppCompatActivity {
         btnUno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveSecondActivity();
+                moveSecondActivity(image1);
             }
         });
 
         btnDos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveSecondActivity();
+                moveSecondActivity(image2);
             }
         });
 
         btnTres.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveSecondActivity();
+                moveSecondActivity(image3);
             }
         });
 
         btnCuatro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveSecondActivity();
+                moveSecondActivity(image4);
             }
         });
     }
 
-    public void moveSecondActivity(){
+    public void moveSecondActivity(int image1){
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("idImg", image1);
         startActivity(intent);
     }
 }
--- app/src/main/java/cl/awakelabs/ejercicio9/SecondActivity.java
+++ app/src/main/java/cl/awakelabs/ejercicio9/SecondActivity.java
@@ -2,13 +2,37 @@ package cl.awakelabs.ejercicio9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        initListeners();
+        Intent intentGet = getIntent();
+        int imageId = intentGet.getIntExtra("idImg", 0);
+        Log.d("SecondActivity", String.valueOf(imageId));
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(imageId);
+    }
+
+    private void initListeners() {
+
+        Button btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
